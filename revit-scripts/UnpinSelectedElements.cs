188cb988990b5eb5278d55a5a6a286c7dcc970ab
baleti <baleti3266@gmail.com>
2025-04-04 16:17:48 +0100

    added UnpinSelectedElements command
    self-explanatory, couldn't find a way to do it out of the box in revit
    
    model we received had too many pinned elements to do it manually one by one

---
+++ revit-scripts/UnpinSelectedElements.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace MyRevitCommands
+{
+    public class UnpinSelectedElements : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get the active document and the selected element IDs
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
+
+            if (selectedIds.Count == 0)
+            {
+                message = "Please select at least one element.";
+                return Result.Failed;
+            }
+
+            // Start a transaction to modify the document
+            using (Transaction tx = new Transaction(doc, "Unpin Selected Elements"))
+            {
+                tx.Start();
+
+                foreach (ElementId id in selectedIds)
+                {
+                    Element element = doc.GetElement(id);
+                    
+                    // Check if the element is pinned and unpin it
+                    if (element.Pinned)
+                    {
+                        element.Pinned = false;
+                    }
+                }
+                
+                tx.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
